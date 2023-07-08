4e2e93ddb64349b7186d547c0a67003582ae531b
Vivek Varma <vsquared101@gmail.com>
2023-07-08 22:13:11 +0530

    Initial commit

---
+++ src/main/java/com/learn/graphql/springgraphqlcrudapp/SpringGraphqlCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.learn.graphql.springgraphqlcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/graphql/springgraphqlcrudapp/SpringGraphqlCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.graphql.springgraphqlcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
